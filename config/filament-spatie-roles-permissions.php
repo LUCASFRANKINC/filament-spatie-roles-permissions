e62f0acbffd5491dfa287f92b27eafa1300f5877
Santhosh Sivan S <86248110+SanthoshSivan-Dev@users.noreply.github.com>
2025-03-28 09:45:22 +0530

    Separate scope_to_tenant for Roles and Permissions (#242)
    * Update filament-spatie-roles-permissions.php
    
    * Update RoleResource.php
    
    * Update PermissionResource.php
    
    * scope to tenant changed to bool

--- config/filament-spatie-roles-permissions.php
+++ config/filament-spatie-roles-permissions.php
@@ -16,6 +16,9 @@ return [
     'team_model' => \App\Models\Team::class,
 
     'scope_to_tenant' => true,
+    
+    'scope_roles_to_tenant' => true,
+    'scope_premissions_to_tenant' => false,
 
     'super_admin_role_name' => 'Super Admin',
 
